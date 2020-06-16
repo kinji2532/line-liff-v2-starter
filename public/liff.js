d0dd203e8bd2832650be5262186e93ed24bd83c2
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2020-06-16 10:29:18 +0900

    Update liff.js

--- public/liff.js
+++ public/liff.js
@@ -1,7 +1,6 @@
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
     const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
-
     // DO NOT CHANGE THIS
     let myLiffId = "";
 
@@ -14,7 +13,7 @@ window.onload = function() {
             })
             .then(function(jsonResponse) {
                 myLiffId = jsonResponse.id;
-                initializeLiffOrDie(myLiffId);
+                initializeLiffOrDie(myLiffId,jsonResponse);
             })
             .catch(function(error) {
                 document.getElementById("liffAppContent").classList.add('hidden');
@@ -30,12 +29,12 @@ window.onload = function() {
 * Check if myLiffId is null. If null do not initiate liff.
 * @param {string} myLiffId The LIFF ID of the selected element
 */
-function initializeLiffOrDie(myLiffId) {
+function initializeLiffOrDie(myLiffId,json) {
     if (!myLiffId) {
         document.getElementById("liffAppContent").classList.add('hidden');
         document.getElementById("liffIdErrorMessage").classList.remove('hidden');
     } else {
-        initializeLiff(myLiffId);
+        initializeLiff(myLiffId,json);
     }
 }
 
@@ -43,14 +42,14 @@ function initializeLiffOrDie(myLiffId) {
 * Initialize LIFF
 * @param {string} myLiffId The LIFF ID of the selected element
 */
-function initializeLiff(myLiffId) {
+function initializeLiff(myLiffId,json) {
     liff
         .init({
             liffId: myLiffId
         })
         .then(() => {
             // start to use LIFF's api
-            initializeApp();
+            initializeApp(json);
         })
         .catch((err) => {
             document.getElementById("liffAppContent").classList.add('hidden');
@@ -61,10 +60,10 @@ function initializeLiff(myLiffId) {
 /**
  * Initialize the app by calling functions handling individual app components
  */
-function initializeApp() {
+function initializeApp(json) {
     displayLiffData();
     displayIsInClientInfo();
-    registerButtonHandlers();
+    registerButtonHandlers(json);
 
     // check if the user is logged in/out, and disable inappropriate button
     if (liff.isLoggedIn()) {
@@ -102,13 +101,10 @@ function displayIsInClientInfo() {
 /**
 * Register event handlers for the buttons displayed in the app
 */
-function registerButtonHandlers() {
+function registerButtonHandlers(json) {
     // openWindow call
     document.getElementById('openWindowButton').addEventListener('click', function() {
-        liff.openWindow({
-            url: 'https://line.me',
-            external: true
-        });
+        window.alert(json);
     });
 
     // closeWindow call
